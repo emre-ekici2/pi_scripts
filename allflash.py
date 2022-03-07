ad28a2ee51c7a8123f7039d5113c8babe72d03a4
Emre Ekici <r0874339@student.thomasmore.be>
2022-03-07 11:22:49 +0100

    fix allflash

--- allflash.py
+++ allflash.py
@@ -3,15 +3,22 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-def blink(pin):
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, 1)
+def blink(pin1, pin2, pin3, pin4):
+    GPIO.setup(pin1, GPIO.OUT)
+    GPIO.setup(pin2, GPIO.OUT)
+    GPIO.setup(pin3, GPIO.OUT)
+    GPIO.setup(pin4, GPIO.OUT)
+    
+    GPIO.output(pin1, 1)
+    GPIO.output(pin2, 1)
+    GPIO.output(pin3, 1)
+    GPIO.output(pin4, 1)
     time.sleep(0.5)
-    while GPIO.output(pin, 1):
-        GPIO.output(23, 1)
-        GPIO.output(24, 1)
-        GPIO.output(25, 1)
-    GPIO.output(pin, 0)
+    
+    GPIO.output(pin1, 0)
+    GPIO.output(pin2, 0)
+    GPIO.output(pin3, 0)
+    GPIO.output(pin4, 0)
     time.sleep(0.5)
 
 for i in range(0,10):
