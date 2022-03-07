96f19d0faf2e132ae6c88fbda5407a9fb89f6732
Emre Ekici <r0874339@student.thomasmore.be>
2022-03-07 10:40:16 +0100

    fix

--- blinkled.py
+++ blinkled.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
 GPIO.setmode(GPIO.BCM)
 
 def blink(pin):
-    GPIO.setup(pin, GPIO.out)
+    GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 1)
-    sleep(0.5)
+    time.sleep(0.5)
     GPIO.output(pin, 0)
-    sleep(0.5)
+    time.sleep(0.5)
 
 for i in range(0,10):
     blink(18)
