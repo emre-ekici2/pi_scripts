11a17f99911412ed6163c72956a4ec3c0c03dab7
Emre Ekici <r0874339@student.thomasmore.be>
2022-03-07 11:16:47 +0100

    fix

--- allflash.py
+++ allflash.py
@@ -7,14 +7,16 @@ def blink(pin):
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 1)
     time.sleep(0.5)
+    while GPIO.output(pin, 1):
+        GPIO.output(23, 1)
+        GPIO.output(24, 1)
+        GPIO.output(25, 1)
     GPIO.output(pin, 0)
     time.sleep(0.5)
 
 for i in range(0,10):
     blink(18)
-    blink(23)
-    blink(24)
-    blink(25)
+
 
 GPIO.cleanup()
 print("Program executed")
